48abe2330641faea544a298945726aa778824f2b
Harrem <Haremmhamad99@gmail.com>
2022-03-13 18:14:51 +0300

    second Commit
    Added Suspension Physics To The Car

--- CarSimulator/Assets/Scripts/Wheel.cs
+++ CarSimulator/Assets/Scripts/Wheel.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Wheel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rb;
+
+
+    [Header("Suspension")]
+    public float restLength;
+    public float springTravel;
+    public float springStiffness;
+    public float damperStiffness;
+
+    float minLength;
+    float maxLength;
+    float lastLength;
+    float springLength;
+    float springVelocity;
+    float springForce;
+    float damperForce;
+
+    Vector3 suspensionForce;
+
+    [Header("Wheel")]
+    public float wheelRaduis;
+
     void Start()
     {
-        
+        rb = transform.root.GetComponent<Rigidbody>();
+
+        minLength = restLength - springTravel;
+        maxLength = restLength + springTravel;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Physics.Raycast(transform.position, -transform.up, 
+            out RaycastHit hit, maxLength + wheelRaduis)){
+
+            lastLength = springLength;
+
+            springLength    = hit.distance - wheelRaduis;
+            springLength    = Mathf.Clamp(springLength, minLength, maxLength);
+            springVelocity  = (lastLength - springLength) / Time.fixedDeltaTime;
+            springForce     = springStiffness * (restLength - springLength);
+            damperForce     = damperStiffness * springVelocity;
+            suspensionForce = (springForce + damperForce) * transform.up;
+
+            rb.AddForceAtPosition(suspensionForce, hit.point);
+        }
     }
 }
